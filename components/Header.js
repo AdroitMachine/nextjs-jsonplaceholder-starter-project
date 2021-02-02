819dd4cf6c928e45165456df226b8e704abc745e
AdroitMachine <anujsaxenamail@gmail.com>
2021-02-02 23:08:08 +0530

    initial commit

---
+++ components/Header.js
@@ -0,0 +1,19 @@
+import headerStyles from '../styles/Header.module.css'
+const Header = () => {
+    return (
+        <div>
+            <h1 className={headerStyles.title}>
+                <span>WebDev</span>News
+            </h1>
+            <p className={headerStyles.description}>keep up to date with latest webdev news</p>
+            {/* <style jsx>
+                {`
+                .title{
+                    color: red
+                }`}
+            </style> */}
+        </div>
+    )
+}
+
+export default Header
---
+++ components/Meta.js
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+
+const Meta = ({ title, keyword, description }) => {
+    return (
+        <Head>
+            <meta name='viewport' content='width=device-width, initial-scale=1' />
+            <meta name='keywords' content={keywords} />
+            <meta name='description' content={description} />
+            <meta charSet='utf-8' />
+            <link rel='icon' href='/favicon.ico' />
+            <title>{title}</title>
+        </Head>
+    )
+}
+Meta.defaultProps = {
+    title: 'WebDev News',
+    keywords: 'webdevelopment, programming,',
+    description: 'Get the latest news'
+}
+export default Meta
